f4b831cce730c0959c197a6301c4162c55ec3403
thewlstudio <thewlstudio@gmail.com>
2026-03-03 23:48:02 +0900

    fix: enable ISR on class page to reflect sanity changes

--- src/app/class/page.tsx
+++ src/app/class/page.tsx
@@ -2,10 +2,10 @@ import { client } from "@/sanity/lib/client";
 import { instructorsQuery } from "@/sanity/lib/queries";
 import ClassClient from "./ClassClient";
 
+export const revalidate = 10; // Refresh data every 10 seconds (ISR)
+
 export default async function ClassIndexPage() {
     const instructors = await client.fetch(instructorsQuery);
 
     return <ClassClient initialInstructors={instructors} />;
 }
-
-
